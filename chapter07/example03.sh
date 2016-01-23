04d64c9607672910ade8128117ee3f5e9d8d5c3f
asvcer <asvcer@aliyun.com>
2016-01-23 14:54:23 +0800

    chapter07

---
+++ chapter07/example03.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Arithmetic tests.
+
+# The (( ... )) construct evaluates and tests numerical expressions.
+# Exit status opposite from [ ... ] construct!
+
+(( 0 ))
+echo "Exit status of \"(( 0 ))\" is $?."      # 1
+
+(( 1 ))
+echo "Exit status of \"(( 1 ))\" is $?."      # 0
+
+(( 5 > 4 ))                                   # true
+echo "Exit status of \"(( 5 > 4 ))\" is $?."  # 0
+
+(( 5 > 9 ))                                   # false
+echo "Exit status of \"(( 5 > 9 ))\" is $?."  # 1
+
+(( 5 - 5 ))                                   # 0
+echo "Exit status of \"(( 5 - 5 ))\" is $?."  # 1
+
+(( 5 / 4 ))                                   # Division o.k.
+echo "Exit status of \"(( 5 / 4 ))\" is $?."  # 0
+
+(( 1 / 2 ))                                   # Division result < 1.
+echo "Exit status of \"(( 1 / 2 ))\" is $?."  # Rounded off to 0.
+                                              # 1
+
+(( 1 / 0 )) 2>/dev/null                       # Illegal division by 0.
+echo "Exit status of \"(( 1 / 0 ))\" is $?."  # 1
+
+exit 0
