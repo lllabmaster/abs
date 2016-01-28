77bc6e441a7eedc0e8536414593baa6f2b71a1b5
asvcer <asvcer@aliyun.com>
2016-01-28 15:33:16 +0800

    chapter09

---
+++ chapter09/example01.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# $IFS treats whitespace differently than other characters.
+
+output_args_one_per_line() {
+
+    for arg
+    do
+	echo "[$arg]"
+    done
+}
+
+echo; echo "IFS=\" \""
+echo "----------"
+
+IFS=" "
+var=" a  b c   "
+output_args_one_per_line $var
+
+echo; echo "IFS=:"
+echo "----------"
+
+IFS=:
+var=":a::b:c:::"
+output_args_one_per_line $var
+
+echo
+
+exit 0
