a82acef6a6bb4e43a23ca2b876917a278b334725
asvcer <asvcer@aliyun.com>
2016-05-14 22:26:07 +0800

    chapter09

--- chapter09/example45.sh
+++ chapter09/example45.sh
@@ -20,5 +20,58 @@ Initialize_Slots() # Zero out all elements of array.
 
 Show_Slots()
 {
+    echo -n " "
+    for i in $( seq $NUMSLOTS )    # Pretty-point array elements.
+    do
+	printf "%3d" ${Slots[$i]}  # Three spaces per result.
+    done
 
+    echo # Row of slots:
+    echo " |__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|"
+    echo "                                ^^"
+    echo
 }
+
+Move()                  # Move one unit right / left, or stay put.
+{
+    Move=$RANDOM        # How random is $RANDOM? Let's see.
+    let "Move %= RANGE" # Normalize into range of 0 - 2.
+    case "$Move" in
+	0) ;;
+	1) (( POS-- ));;
+	2) (( POS++ ));;
+	*) echo -n "Error ";;
+    esac
+}
+
+Play()
+{
+    i=0
+    while [ $i -lt $ROWS ]
+    do
+	Move
+	(( i++ ));
+    done
+
+    SHIFT=11
+    let "POS += SHIFT"
+    (( Slots[$POS]++ ))
+}
+
+Run()
+{
+    p=0
+    while [ $p -lt $PASSES ]
+    do
+	Play
+	(( p++ ))
+	POS=0
+    done
+}
+
+# main
+Initialize_Slots
+Run
+Show_Slots
+
+exit $?
