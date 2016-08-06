a6c02f588659897ea9794e2b98d26dc9f3431d80
asvcer <asvcer@aliyun.com>
2016-08-06 18:07:19 +0800

    fixed a bug

--- chapter15/example42.sh
+++ chapter15/example42.sh
@@ -3,14 +3,13 @@
 # Show available ip in a specific local lan.
 # @author: andy
 
-COUNT=3
+COUNT=1
 
-for i in $(seq 254)
+for i in $(seq 100)
 do
     ping -c $COUNT "219.223.169.$i" > /dev/null
-    result=$?
 
-    if [ $result -eq 0 ]
+    if [ $? -eq 0 ]
     then
 	echo "219.223.169.$i"
     fi
