1d71569ce71fb7b17b2b387661e7213e1b68bcd8
asvcer <asvcer@aliyun.com>
2016-08-13 16:08:42 +0800

    chapter15

---
+++ chapter15/example48.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Counting letter occurrences in a text file.
+
+MINARGS=2         # Script requires at least two arguments.
+E_BADARGS=65
+FILE=$1
+
+let LETTERS=$#-1  # How many letters specified (as command-line args).
+                  # (Substract 1 from number of command line args.)
+
+show_help()
+{
+    echo
+    echo "Usage: `basename $0` file letters."
+    echo "Note: `basename $0` arguments are case sensitive."
+    echo "Example: `basename $0` foobar.txt G n U L i N U x."
+    echo
+}
+
+# Checks number of arguments.
+if [ $# -lt $MINARGS ]
+then
+    echo
+    echo "Not enough arguments."
+    echo
+    show_help
+    exit $E_BADARGS
+fi
+
+# Checks if file exists.
+if [ ! -f $FILE ]
+then
+    echo "File \"$FILE\" does not exist."
+    exit $E_BADARGS
+fi
+
+# Counts letter occurrences.
+for n in $(seq $LETTERS)
+do
+    shift
+    if [[ `echo -n "$1" | wc -c` -eq 1 ]]                # Checks arg.
+    then
+	echo "$1" -\> `cat $FILE | tr -cd "$1" | wc -c`  # Counting.
+    else
+	echo "$1 is not a single char."
+    fi
+done
+
+exit $?
---
+++ chapter15/example49.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+mkfifo pipe1
+mkfifo pipe2
+
+(cut -d' ' -f1 | tr "a-z" "A-Z") > pipe2 < pipe1 &
+ls -1 | tr -s ' ' | cut -d' ' -f3,9- | tee pipe1 | cut -d' ' -f2 | paste - pipe2
+
+rm -f pipe1
+rm -f pipe2
+
+exit $?
