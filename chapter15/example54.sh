7c9b86f03a6e6b48e9950da183dbfb61424f36ff
asvcer <asvcer@aliyun.com>
2016-08-15 15:32:53 +0800

    chapter15

---
+++ chapter15/example54.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Temp filename generator.
+
+BASE_STR=`mcookie`
+POS=11
+LEN=5
+
+prefix="temp"
+suffix=${BASE_STR:POS:LEN}
+
+temp_filename=$prefix.$suffix
+
+echo "Temp filename = "$temp_filename""
+
+exit 0
